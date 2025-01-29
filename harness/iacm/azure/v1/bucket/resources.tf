c604f1147f2bb156ad37f5d4096a64713281a725
Joe Titra <jtitra@harness.io>
2025-01-29 11:27:49 -0500

    updated azure config

--- harness/iacm/azure/v1/bucket/resources.tf
+++ harness/iacm/azure/v1/bucket/resources.tf
@@ -1,17 +1,15 @@
 // Define the resources to create
 // Provisions the following into Azure: 
-//    Blob Storage
+//    Blob Storage Container
 
-// Blob Storage
-resource "azurerm_storage_blob" "se_bucket" {
-  name                   = var.bucket_name
-  storage_account_name   = "harnessidpdemo"
-  storage_container_name = "demo"
-  type                   = "Block"
+// Blob Storage Container
+resource "azurerm_storage_container" "se_bucket" {
+  name                  = var.bucket_name
+  storage_account_name  = "harnessidpdemo"
+  container_access_type = "private"
 
-//  metadata = {
-//    Name        = var.bucket_name
-//    Owner       = var.bucket_owner
-//    Environment = "se-demo"
-//  }
+  metadata = {
+    owner       = var.bucket_owner
+    environment = "se-demo"
+  }
 }
